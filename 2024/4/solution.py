efc3b6fe042b033da3ef861d59bf62ac64d388ea
jkbngl <jakob.engl@hotmail.de>
2024-12-05 23:42:17 +0100

    one gold start, day #4 - with one day delay

--- 2024/4/solution.py
+++ 2024/4/solution.py
@@ -1,6 +1,8 @@
 import re
 
-filename = './input_example.txt'
+# filename = './input_example.txt'
+# filename = './input_example_easy.txt'
+filename = './input.txt'
 
 with open(filename) as file:
     lines = [line.rstrip() for line in file]
@@ -22,7 +24,8 @@ total_matches = 0
 count_rows = len(array_3d)
 count_column = len(array_3d[0])
 
-print(count_rows)
+print(f"count_rows: {count_rows}")
+print(f"count_column: {count_column}")
 
 row_matches = 0
 # in same row
@@ -35,48 +38,76 @@ column_matches = 0
 # in same column
 for column in range(count_column):
     str = [array_3d[row][column] for row in range(len(array_3d))]
-    print(''.join(str))
     column_matches += len(re.findall(pattern, ''.join(str)))
 total_matches += column_matches
 print(f"Column matches: {column_matches}")
 
-diagonal_matches_fr = 0
-for i in range(9, -1, -1):
-    str = ""
-    for j in range(10):
-        try:
-            val = array_3d[i+j][j]
-
-            str += val
-            # print(f"i: {i+j}, j: {j} -> {val}")
-        except:
-            pass
-    
-    print("str: ", str)
-
-    diagonal_matches_fr += len(re.findall(pattern, str))
-
-print(f"Diagonal matches: {column_matches}")
-
-total_matches += diagonal_matches_fr
-
 diagonal_matches_rl = 0
-for i in range(9, -1, -1):
-    str = ""
-    for j in range(10):
+for i in range(count_rows, -1, -1):
+    str1 = ""
+    str2 = ""
+    for j in range(count_rows):
         try:
             val = array_3d[i+j][j]
 
-            str += val
-            # print(f"i: {i+j}, j: {j} -> {val}")
+            str1 += val
+        except:
+            pass
+
+        try:
+            val = array_3d[j][i+j]
+
+            # the middle line only once, not from both sides
+            if i != 0:
+                str2 += val
         except:
             pass
     
-    print("str: ", str)
+    # print(f"str1: {str1} -> {len(re.findall(pattern, str1))}")
+    # print(f"str2: {str2} -> {len(re.findall(pattern, str2))}")
 
-    diagonal_matches_fr += len(re.findall(pattern, str))
+    diagonal_matches_rl += len(re.findall(pattern, str1))
+    diagonal_matches_rl += len(re.findall(pattern, str2))
 
-print(f"Diagonal matches: {column_matches}")
+print(f"Diagonal matches_rl: {diagonal_matches_rl}")
 
-total_matches += diagonal_matches_fr
+diagonal_matches_lr = 0
+for i in range(count_rows):
+    str1 = ""
+    str2 = ""
+    for j in range(count_rows):
+
+        try:
+            val = array_3d[i+j][(count_rows-1)-j]
+
+            str1 += val
+        except:
+            pass
+
+        try:
+            if (i-j) >= 0:
+                val = array_3d[i-j][j]
+
+                # the middle line only once, not from both sides
+                if i != (count_rows-1):
+                    str2 += val
+        except:
+            pass
+
+
+    print(f"str1: {str1} -> {len(re.findall(pattern, str1))}")
+    print(f"str2: {str2} -> {len(re.findall(pattern, str2))}")
+
+    diagonal_matches_lr += len(re.findall(pattern, str1))
+    diagonal_matches_lr += len(re.findall(pattern, str2))
+
+print(f"Diagonal matches_lr: {diagonal_matches_lr}")
+
+total_matches += diagonal_matches_rl
+total_matches += diagonal_matches_lr
 print(f"Total matches: {total_matches}")
+
+
+# 2316 -> That's not the right answer; your answer is too low
+# 2404 -> That's not the right answer; your answer is too high
+# 2397 -> That's the right answer! You are one gold star closer to saving your vacation.
