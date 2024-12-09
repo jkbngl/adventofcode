064704d59bde8d240d2d2d6acc65cf0992cfeec2
Jakob Engl <32094260+jkbngl@users.noreply.github.com>
2024-12-09 11:29:57 +0100

    few further unsuccessfull tests

--- 2024/5/solution.py
+++ 2024/5/solution.py
@@ -2,7 +2,7 @@ import math
 
 filename = './input_example.txt'
 # filename = './input_example_easy.txt'
-# filename = './input.txt'
+filename = './input.txt'
 
 with open(filename) as file:
     lines = [line.rstrip() for line in file]
@@ -18,69 +18,71 @@ for j, page in enumerate(pages):
 
     print(f"Sorting: {pairs}")
 
-    for i, pair in enumerate(pairs):
+    # for i, pair in enumerate(pairs):
+    i = 0
+    while i < len(pairs):
         # print("Checking pair at index ", i)
         # Last one not compared
-        if i == len(pairs) - 1:
-            break
+        # if i == len(pairs):
+            # break
 
         firstnum = pairs[i]
-        secondnum = pairs[i+1]
 
-        ruleA = rules.get(f"{firstnum}|{secondnum}")
-        ruleB = rules.get(f"{secondnum}|{firstnum}")
+        swap_done = False
 
-        rule = ruleA if ruleA else ruleB
+        k = i
+        while k < len(pairs):
+            secondnum = pairs[k]
 
-        if rule:
-            print(f"Comparing {firstnum} with {secondnum} -> {rule}")
+            ruleA = rules.get(f"{firstnum}|{secondnum}")
+            ruleB = rules.get(f"{secondnum}|{firstnum}")
 
-            firstrule = rule.split('|')[0]
-            secondrule = rule.split('|')[1]
+            rule = ruleA if ruleA else ruleB
 
-            if firstnum != firstrule:
-                print(f"-> Swapping {firstnum} with {firstrule}")
-                pairs[i], pairs[i+1] = pairs[i+1], pairs[i]
-                print(f"-> New page: {pairs}")
+            if rule:
+                # print(f"Comparing {firstnum} with {secondnum} -> {rule}")
+
+                firstrule = rule.split('|')[0]
+                secondrule = rule.split('|')[1]
+
+                if firstnum != firstrule:
+                    # print(f"-> Swapping {firstnum} with {firstrule}")
+                    pairs[i], pairs[k] = pairs[k], pairs[i]
+                    # k = i
+                    # swap_done = True
+
+                    k += 1
+
+                    # print(f"New pairs: {pairs}")
+                else:
+                    k += 1
+                    # print(f"{firstnum} and {secondnum} are already in the correct order")
+                    pass
             else:
-                print(f"{firstnum} and {secondnum} have no rule")
-                pass
-        
-        print("+++++")
-        # Recheck the same after switch
-        firstnum = pairs[i]
-        secondnum = pairs[i+1]
+                k += 1
+            
 
-        ruleA = rules.get(f"{firstnum}|{secondnum}")
-        ruleB = rules.get(f"{secondnum}|{firstnum}")
+            print(k)
 
-        rule = ruleA if ruleA else ruleB
+        # if swap_done:
+        #     i = 0
+        # else:
+        #     i += 1
+        i += 1
 
-        if rule:
-            print(f"Comparing {firstnum} with {secondnum} -> {rule}")
-
-            firstrule = rule.split('|')[0]
-            secondrule = rule.split('|')[1]
-
-            if firstnum != firstrule:
-                print(f"Swapping {firstnum} with {firstrule}")
-                pairs[i], pairs[i+1] = pairs[i+1], pairs[i]
-            else:
-                print(f"{firstnum} and {secondnum} have no rule")
-                pass
-                
     print("Sorted: ", pairs)
     print("-------------------")
     pages[j] = pairs
 
 res = 0
-# print('##############')
-# for page in pages:
-#     res += int(page[math.floor(len(page)/2)])
-#     print(f"Page: {page} -> {page[math.floor(len(page)/2)]}")
+print('##############')
+for page in pages:
+    res += int(page[math.floor(len(page)/2)])
+    print(f"Page: {page} -> {page[math.floor(len(page)/2)]}")
 
 
-# print(f"Result: {res}")
+print(f"Result: {res}")
 
 
 # 10890 your answer is too high
+# 10562 your answer is too high
